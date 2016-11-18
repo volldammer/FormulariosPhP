9cb82db704b898863ccc5bb56921a12b9a069600
volldammer <oskarwhite@gmail.com>
2016-11-18 16:37:04 +0100

    Creamos un proyecto con un formulario de alta completo.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!--
+ejemplo de un formulario un poquito mas completo
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejemplode formulario de alta</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
